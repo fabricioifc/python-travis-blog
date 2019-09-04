0f2d6258fb05bfed2412e27ef69cb2a91f7fb69c
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2019-09-04 20:19:16 -0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e # termina o script com um código diferente de 0 se alguma coisa falhar
+
+SOURCE_BRANCH="master"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
